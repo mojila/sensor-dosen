b70809ec0fd9d62a08691a6940de475b4b889a3b
mojila <me.mojila@gmail.com>
2019-10-23 00:07:47 +0700

    add disable setuid

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function writeStatusData(data) {
 }
 
 async function run () {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     let data = [];
     let results = [];
